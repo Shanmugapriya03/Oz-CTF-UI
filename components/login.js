4673fca9fac5d4f647a1b75ef07ab8d73a6309e2
priya <priya97a@gmail.com>
2017-07-01 14:14:33 +0530

    debug commit

--- components/login.js
+++ components/login.js
@@ -1,26 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component{
     constructor(){
         super();
-        this.state= {name:""}
+        this.state= {name:"",pass:""}
         this.setName = this.setName.bind(this);
+        this.setPass = this.setPass.bind(this);
         this.show = this.show.bind(this);
     }
     setName(e){
 
         var username = e.target.value;
-        this.setState({name:username})
-        alert(this.state.name);
+        this.setState({name:username});
+
+    }
+    setPass(e){
+        var password=e.target.value;
+        this.setState({pass:password});
+
     }
     show(){
-        alert(this.state);
+        axios.post('/login', {
+            username: this.state.name,
+            password: this.state.pass
+        })
+          .then(function (response) {
+            console.log(response);
+        })
+          .catch(function (error) {
+            console.log(error);
+        });
+        console.log(this.state);
     }
     render(){
-        console.log(11111);
         return (
             <div>
                 <input type="text" placeholder="enter ozmenta id" onChange={this.setName}/>
+                <input type="password" placeholder="password" onChange={this.setPass}/>
                 <button type="submit" onClick={this.show}>Login</button>
             </div>
         );
