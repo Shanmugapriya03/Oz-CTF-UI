594ee02a2480961b6ef000ceb5712568d354be9b
priya <priya97a@gmail.com>
2017-07-02 19:08:45 +0530

    modified func

--- components/dashboard.js
+++ components/dashboard.js
@@ -5,7 +5,7 @@ export default class Dashboard extends React.Component{
         super();
         this.state = {
             challenges: [
-                {name:"ch1", score:"100"},{name:"ch2", score:"1000"},{name:"ch3",score:"500"}
+                {name:"ch1", score:"100" ,content:"challenges--1"},{name:"ch2", score:"1000" ,content:"challenges--2"},{name:"ch3",score:"500" ,content:"challenges--3"}
             ],
             selected:null
         };
@@ -13,12 +13,13 @@ export default class Dashboard extends React.Component{
 
         this.renderItems = this.renderItems.bind(this);
         this.renderContent = this.renderContent.bind(this);
+        this.renderScore = this.renderScore.bind(this);
         this.setSelected = this.setSelected.bind(this);
     }
     renderItems(){
         var items=[];
         for (let i in this.state.challenges){
-            items.push(<ChallengeItem name={this.state.challenges[i].name} score={this.state.challenges[i].name} setSelected={this.setSelected}/>);
+            items.push(<ChallengeItem name={this.state.challenges[i].name} score={this.state.challenges[i].score} content={this.state.challenges[i].content} setSelected={this.setSelected}/>);
         }
         return items;
     }
@@ -30,6 +31,13 @@ export default class Dashboard extends React.Component{
             );
         }
         return null;
+    }
+    renderScore(){
+        var tot=0;
+        for(let i in this.state.challenges){
+            tot+=Number(this.state.challenges[i].score);
+        }
+        return tot;
     }
     setSelected(val){
         this.setState({selected:val});
@@ -43,8 +51,13 @@ export default class Dashboard extends React.Component{
                             {this.renderItems()}
                         </div>
 
-                        <div className="col-md-9">
+                        <div className="col-md-7">
                             {this.renderContent()}
+                        </div>
+
+                        <div className="col-md-2">
+                            <b>Total Score</b>
+                            {this.renderScore()}
                         </div>
                     </div>
                 </div>
