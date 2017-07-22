02a019e2340d4a271380f9a538b10d5ff3c72f34
priya <priya97a@gmail.com>
2017-07-22 20:22:48 +0530

    changes made

--- components/challengeItem.js
+++ components/challengeItem.js
@@ -4,7 +4,7 @@ export default class ChallengeItem extends React.Component {
     render(){
         return (
             <div>
-                <h3 onClick={()=>this.props.setSelected(this.props.content)}>{this.props.name}</h3>
+                <h3 onClick={()=>this.props.setSelected(this.props.content,this.props.question)}>{this.props.name}</h3>
                 <span>{this.props.score}</span>
             </div>
         );
--- components/dashboard.js
+++ components/dashboard.js
@@ -6,9 +6,12 @@ export default class Dashboard extends React.Component{
         super();
         this.state = {
             challenges: [
-                {name:"ch1", score:"100" ,content:"challenges--1"},{name:"ch2", score:"1000" ,content:"challenges--2"},{name:"ch3",score:"500" ,content:"challenges--3"}
+                {name:"ch1", score:"100" ,content:"challenges--1",question:"Question----1 goes here........."},
+                {name:"ch2", score:"1000" ,content:"challenges--2",question:"Question----2 goes here........."},
+                {name:"ch3",score:"500" ,content:"challenges--3",question:"Question----3 goes here........."}
             ],
-            selected:null
+            selected:null,
+            question:null
         };
 
         this.renderItems = this.renderItems.bind(this);
@@ -19,14 +22,21 @@ export default class Dashboard extends React.Component{
     renderItems(){
         var items=[];
         for (let i in this.state.challenges){
-            items.push(<ChallengeItem name={this.state.challenges[i].name} score={this.state.challenges[i].score} content={this.state.challenges[i].content} setSelected={this.setSelected}/>);
+            items.push(<ChallengeItem name={this.state.challenges[i].name} score={this.state.challenges[i].score} content={this.state.challenges[i].content} question={this.state.challenges[i].question} setSelected={this.setSelected}/>);
         }
         return items;
     }
     renderContent(){
         if (this.state.selected != null){
             return (
-                <h1> {this.state.selected }</h1>
+                <div>
+                    <h1> {this.state.selected }</h1>
+                    <p>{this.state.question}</p>
+                        <div class="form-group">
+                            <label for="comment">Answer:</label>
+                            <textarea class="form-control" rows="10" cols="10" id="comment"></textarea>
+                        </div>
+                </div>
             );
         }else{
 			return (
@@ -41,8 +51,8 @@ export default class Dashboard extends React.Component{
         }
         return tot;
     }
-    setSelected(val){
-        this.setState({selected:val});
+    setSelected(val1,val2){
+        this.setState({selected:val1,question:val2});
     }
     render(){
         return (
