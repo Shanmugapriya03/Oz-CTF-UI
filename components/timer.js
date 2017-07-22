5b6e657bd9af1858cdf3d07e89eb9e48c3af7275
shivakishore14 <shivakishore14@gmail.com>
2017-07-22 22:32:47 +0530

    timer changes

--- components/timer.js
+++ components/timer.js
@@ -2,23 +2,44 @@ import React from 'react';
 export default class Timer extends React.Component{
     constructor(){
             super();
+			this.state = {currentHour:0, currentMin:0, currentSec:0, endTime:"2017-07-22T16:44:06.970Z"}
+			this.updateTimer = this.updateTimer.bind(this);
+			this.setDummyEndDate = this.setDummyEndDate.bind(this);
     };
+
+
+	componentWillMount(){
+		this.setDummyEndDate();
+		setInterval(this.updateTimer, 1000);
+	}
+	setDummyEndDate(){
+		let current = new Date();
+		let newTime = current.getTime() + 1000 * 3600 * 2; //add 2 hrs
+		let dummyEnd = new Date(newTime);
+		this.setState({endTime : dummyEnd.toISOString()})
+	}
+	updateTimer(){
+		let endTime = new Date( this.state.endTime );
+		let curTime = new Date();
+		let timeDiff = endTime.getTime() - curTime.getTime();
+
+		if ( timeDiff <= 0 ){
+			console.log("time finised");
+			return;
+		}
+
+		let dayDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+		let hourDiff = Math.floor( timeDiff / (1000 * 3600) ) % 24;
+		let minDiff = Math.floor( timeDiff / (1000 * 60) ) % 60;
+		let secDiff = Math.floor( timeDiff / (1000) ) % 60;
+		this.setState({currentHour: hourDiff, currentMin: minDiff, currentSec:secDiff});
+
+	}
     render(){
-        var countdown =  120* 60 * 1000;
-        var timerId = setInterval(function(){
-            countdown -= 1000;
-            var min = Math.floor(countdown / (60 * 1000));
-            var sec = Math.floor((countdown -(min * 60 * 1000))/ 1000);
-            if (countdown <=0) {
-                alert("time over!!");
-                clearInterval(timerId);
-            } else {
-                $("#countTime").html(min + "mins : " + sec+" secs");
-            }
-        }, 1000);
+
         return(
             <div>
-                <p id="countTime"></p>
+                <p>{this.state.currentHour}H: {this.state.currentMin}M: {this.state.currentSec}S</p>
             </div>
         );
     }
